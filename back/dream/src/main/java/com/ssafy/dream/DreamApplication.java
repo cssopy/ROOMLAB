c2e3d8dc9024187a7002f0bb27234bafe9838ad5
Juwon-Kiim <sa6812000@naver.com>
2022-10-19 21:22:08 +0900

    init

---
+++ back/dream/src/main/java/com/ssafy/dream/DreamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamApplication.class, args);
+	}
+
+}
