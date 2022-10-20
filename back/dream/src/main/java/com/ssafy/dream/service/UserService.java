fe17645af5247d341036e6637ad7eea37c10a49c
Juwon-Kiim <sa6812000@naver.com>
2022-10-20 16:56:41 +0900

    [BE] 생성 : 회원탈퇴 api 구현 (#2)

--- back/dream/src/main/java/com/ssafy/dream/service/UserService.java
+++ back/dream/src/main/java/com/ssafy/dream/service/UserService.java
@@ -1,6 +1,6 @@
 package com.ssafy.dream.service;
 
-import com.ssafy.dream.dto.req.ReqSignupDto;
+import com.ssafy.dream.dto.req.ReqUserDto;
 import com.ssafy.dream.entity.Users;
 import com.ssafy.dream.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,16 +20,29 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public ResponseEntity<?> signUp(ReqSignupDto reqSignupDto){
-        if(userRepository.existsByUserId(reqSignupDto.getUserId())){
+    public ResponseEntity<?> signUp(ReqUserDto reqUserDto){
+        if(userRepository.existsByUserId(reqUserDto.getUserId())){
             return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
         }
         Users user = Users.builder()
-                .userId(reqSignupDto.getUserId())
-                .userPwd(passwordEncoder.encode(reqSignupDto.getUserPwd()))
+                .userId(reqUserDto.getUserId())
+                .userPwd(passwordEncoder.encode(reqUserDto.getUserPwd()))
                 .roles(Collections.singletonList("ROLE_USER"))
                 .build();
         userRepository.save(user);
         return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> withdrawal(ReqUserDto reqUserDto){
+        Users user = userRepository.findByUserId(reqUserDto.getUserId());
+        if(user != null){
+            if(passwordEncoder.matches(reqUserDto.getUserPwd(), user.getUserPwd())){
+                userRepository.delete(user);
+
+                return new ResponseEntity<>(true, HttpStatus.OK);
+            }
+            else return new ResponseEntity<>("비밀번호가 틀렸습니다", HttpStatus.BAD_REQUEST);
+        }
+        else return new ResponseEntity<>("존재하지 않는 유저입니다", HttpStatus.BAD_REQUEST);
     }
 }
