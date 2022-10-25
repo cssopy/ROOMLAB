885c0b443c8c25e2efa1afc263da2439b52b14cf
mosquito0076 <hsmk0076@naver.com>
2022-10-25 16:21:08 +0900

    [WEB] 생성 : WEB 브랜치 생성

---
+++ web/src/setupProxy.js
@@ -0,0 +1,23 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/back", {
+      target: "http://localhost:8080",
+      pathRewrite: {
+        "^/back": "",
+      },
+      changeOrigin: true,
+    })
+  );
+
+  // app.use(
+  //   createProxyMiddleware('/다른context', {
+  //     target: 'https://다른호스트',
+  //     pathRewrite: {
+  //       '^/지우려는패스':''
+  //     },
+  //     changeOrigin: true
+  //   })
+  // )
+};
