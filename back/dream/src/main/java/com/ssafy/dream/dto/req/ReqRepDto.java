bb8a790f35e70357f5be8e3f325fb27a6f1e74a5
mosquito0076 <hsmk0076@naver.com>
2022-11-01 16:23:25 +0900

    [BE] 기타: : 세부사항 완성 완료

--- back/dream/src/main/java/com/ssafy/dream/dto/req/ReqRepDto.java
+++ back/dream/src/main/java/com/ssafy/dream/dto/req/ReqRepDto.java
@@ -3,10 +3,13 @@ package com.ssafy.dream.dto.req;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class ReqRepDto {
     private Long userIdx;
     private Long expIdx;
     private String repContent;
     private int repScore;
+    private List<String> repAnswers;
 }
--- back/dream/src/main/java/com/ssafy/dream/entity/Reports.java
+++ back/dream/src/main/java/com/ssafy/dream/entity/Reports.java
@@ -1,12 +1,14 @@
 package com.ssafy.dream.entity;
 
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Builder
@@ -30,9 +32,37 @@ public class Reports {
     private Users userIdx;
 
     @Column(name = "rep_content", columnDefinition = "text")
+    @NotNull
     private String repContent;
 
     @Column(name = "rep_score")
+    @NotNull
     private int repScore;
+
+    @Column(name = "rep_answer_1")
+    @NotNull
+    private String repAnswer1;
+
+    @Column(name = "rep_answer_2")
+    @NotNull
+    private String repAnswer2;
+
+    @Column(name = "rep_answer_3")
+    @NotNull
+    private String repAnswer3;
+
+    @Column(name = "rep_answer_4")
+    @NotNull
+    private String repAnswer4;
+
+    @Column(name = "rep_answer_5")
+    @NotNull
+    private String repAnswer5;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "rep_date")
+    @NotNull
+    private Date repDate;
+
 
 }
