4a582d4d41539265e0f8a11828bc1181a281a8af
mosquito0076 <hsmk0076@naver.com>
2022-11-03 15:22:06 +0900

    [WEB] 수정 : url 수정

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/web" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
