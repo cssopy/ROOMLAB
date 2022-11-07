713664c02d97ef4b8230c794be9eddd763c19402
cssopy <83584721+cssopy@users.noreply.github.com>
2022-11-07 17:59:19 +0900

    [UNITY] 생성 : 실험 구현 (#14)

--- unity/Exp_FlameTest/Scripts/Pipette.cs
+++ unity/Exp_FlameTest/Scripts/Pipette.cs
@@ -12,6 +12,8 @@ public class Pipette : GrabbableEvents
     private Renderer glassLiquidRenderer;
     private bool isInteractingWithSample = false;
 
+    Collider other = null;
+
     private void Awake()
     {
         glassLiquidRenderer = glassLiquid.GetComponent<Renderer>();
@@ -24,7 +26,7 @@ public class Pipette : GrabbableEvents
             float colour = glassLiquidRenderer.material.GetFloat("_FillAmount");
             if (colour < 0.581)
             {
-                colour += 0.01f;
+                colour += 0.001f;
                 glassLiquidRenderer.material.SetFloat("_FillAmount", colour);
                 StartCoroutine(playTrickle());
             }
@@ -32,49 +34,14 @@ public class Pipette : GrabbableEvents
             {
                 sampleName = null;
             }
-        }
-    }
-
-    /*public void OnActivated()
-    {
-        if (!isInteractingWithSample)
-        {
-            float colour = glassLiquidRenderer.material.GetFloat("_FillAmount");
-            if (colour < 0.581)
-            {
-                colour += 0.01f;
-                glassLiquidRenderer.material.SetFloat("_FillAmount", colour);
-                StartCoroutine(playTrickle());
-            }
-            else
-            {
-                sampleName = null;
-            }
-        }
-    }*/
-
-    private IEnumerator playTrickle()
-    {
-        trickle.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        trickle.SetActive(false);
-    }
-
-    public void OnTriggerEnterInGlass()
-    {
-        isInteractingWithSample = true;
-    }
-
-    public void OnTriggerStayInGlass(Collider other)
-    {
-        if (isInteractingWithSample)
+        }else if (isInteractingWithSample && other != null)
         {
             sampleName = other.name;
-            if(other.name == "Cu")
+            if (other.name == "Cu")
             {
                 glassLiquidRenderer.material.SetColor("_Colour", new Color(118f / 255f, 183f / 255f, 163f / 255f, 255f / 255f));
             }
-            else if(other.name == "Ba")
+            else if (other.name == "Ba")
             {
                 glassLiquidRenderer.material.SetColor("_Colour", new Color(105f / 255f, 104f / 255f, 125f / 255f, 60f / 255f));
             }
@@ -86,20 +53,33 @@ public class Pipette : GrabbableEvents
             float colour = glassLiquidRenderer.material.GetFloat("_FillAmount");
             if (colour > 0.4)
             {
-                colour += -0.01f;
+                colour += -0.001f; 
                 glassLiquidRenderer.material.SetFloat("_FillAmount", colour);
             }
         }
     }
 
+    private IEnumerator playTrickle()
+    {
+        trickle.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        trickle.SetActive(false);
+    }
+
+    public void OnTriggerEnterInGlass(Collider other)
+    {
+        isInteractingWithSample = true;
+        this.other = other;
+    }
+
     public void OnTriggerExitInGlass()
     {
         isInteractingWithSample = false;
+        this.other = null;
     }
 
     public void OnParticleCollisionAtTrickle(GameObject other)
     {
-        Debug.Log("부모 함수 진입");
         other.GetComponent<Cotton>().type = sampleName;
     }
 }
